7f72ac375b15e9f1e5e50716bc6c3228b065e629
Peter Salhofer <peter.salhofer@fh-joanneum.at>
2016-12-05 16:12:42 +0100

    loading dialog now created locally in function

--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
-import {NavController, NavParams, AlertController, LoadingController, Loading} from 'ionic-angular';
+import {NavController, NavParams, AlertController, LoadingController} from 'ionic-angular';
 
 import {Security} from '../../providers/security';
 import {LoginPage} from "../login/login";
@@ -10,7 +10,6 @@ import {AddTripPage} from "../add-trip/add-trip";
 import {TripPage} from "../trip/trip";
 
 
-
 @Component({
   templateUrl: 'list.html'
 })
@@ -18,44 +17,51 @@ export class ListPage {
   selectedItem: any;
   icons: string[];
   items: Array<Trip>;
-  loading: Loading;
-
 
   constructor(public navCtrl: NavController,
               public navParams: NavParams,
               private security: Security,
               private tLogService: Tlog,
               private alertCtrl: AlertController,
-              private loadingCtrl: LoadingController
-  ) {
+              private loadingCtrl: LoadingController) {
     // If we navigated to this page, we will have an item available as a nav param
     this.selectedItem = navParams.get('item');
-    this.loading = loadingCtrl.create({
-      content: "Fetching your trips"
-    });
     this.items = [];
 
   }
 
 
-
   addTrip = () => this.navCtrl.push(AddTripPage)
 
-  showAlert = (title:string,message:string) => this.alertCtrl.create({title: title, message: message, buttons: ['OK']}).present();
+  showAlert = (title: string, message: string) => this.alertCtrl.create({
+    title: title,
+    message: message,
+    buttons: ['OK']
+  }).present();
 
-  loadTrips = () => this.loading.present()
-    .then(this.tLogService.getTrips)
-    .then(trips => this.items=trips).then(() => {
-      this.loading.dismiss();
-      if (this.items.length === 0) {this.showAlert("INFO","You do not have any trips yet. Press the Plus Icon to create one.") }
+  loadTrips = () => {
+    let loading = this.loadingCtrl.create({
+      content: "Fetching your trips"
     })
-    .catch(err => {
-      this.loading.dismiss();
-      this.showAlert("Error",`Could not retrieve list of trips: ${err.message || err}`);
-    });
+    loading.present()
+      .then(this.tLogService.getTrips)
+      .then(trips => this.items = trips)
+      .then(() => {
+        loading.dismiss();
+        if (this.items.length === 0) {
+          this.showAlert("INFO", "You do not have any trips yet. Press the Plus Icon to create one.")
+        }
+      })
+      .catch(err => {
+        loading.dismiss();
+        this.showAlert("Error", `Could not retrieve list of trips: ${err.message || err}`);
+      });
+  }
 
   ionViewWillEnter = () => {
-    this.security.isNotloggedIn().then(exp => {if (exp) this.navCtrl.setRoot(LoginPage); else this.loadTrips()});
+    this.security.isNotloggedIn().then(exp => {
+      if (exp) this.navCtrl.setRoot(LoginPage); else this.loadTrips()
+    });
   }
 
   itemTapped(event, tripID) {
