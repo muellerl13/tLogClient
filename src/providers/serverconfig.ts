b737a4cea1553a6179086a473740012d9fbc3d75
eggerand13 <andreas.egger@edu.fh-joanneum.at>
2017-02-03 13:07:40 +0100

    initial

--- src/providers/serverconfig.ts
+++ src/providers/serverconfig.ts
@@ -10,7 +10,7 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class Serverconfig {
 
-  private _host = "http://10.22.1.112:3000/api";
+  private _host = "http://25.89.179.89:3000/api";
   private _loginURI = `${this._host}/auth/login`;
   private _registerURI = `${this._host}/auth/signup`;
   private _mineURI = `${this._host}/trip/mine`;
